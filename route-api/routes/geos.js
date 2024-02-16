427d3fc0e426cd05fa32b4d479ad50f2378a653a
Sprinter05 <sprinterhr@gmail.com>
2024-02-17 00:55:08 +0100

    Implemented calc_distance API method

--- route-api/routes/geos.js
+++ route-api/routes/geos.js
@@ -1,5 +1,6 @@
 const nodeGeocoder = require('node-geocoder')
 const geocoder = nodeGeocoder({provider: 'openstreetmap'})
+const geolib = require('geolib')
 
 exports.getPlaceInfo = async function(origin){
     const result = await geocoder.geocode(origin)
@@ -9,4 +10,12 @@ exports.getPlaceInfo = async function(origin){
         "loc": `${result[0].formattedAddress}`
     }
     return jsonOutput
+}
+
+exports.getDistance = async function(og, dt){
+    const distMeters = geolib.getDistance(
+        { latitude: og.lat, longitude: og.len },
+        { latitude: dt.lat, longitude: dt.len }
+    )
+    return distMeters
 }
--- route-api/routes/router.js
+++ route-api/routes/router.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const { getPlaceInfo } = require('./geos')
+const { getPlaceInfo, getDistance } = require('./geos')
 
 router.get('/', function(req, res) {    
     res.json(
@@ -11,12 +11,23 @@ router.get('/', function(req, res) {
 })
 
 router.get('/get_place', async function(req, res) {
-    if(req.query.name === undefined || req.query.name.length === 0) return res.status(401).send("Error 401")
-    const placeInfo = await getPlaceInfo(req.query.name)
+    rQ = req.query
+    if(rQ.name === undefined || rQ.name.length === 0) {
+        return res.status(401).send("Error 401")
+    }
+    const placeInfo = await getPlaceInfo(rQ.name)
     res.json(placeInfo)
     return
 })
 
-router.get('/calc_distance', async function(req, res) {})
+router.get('/calc_distance', async function(req, res) {
+    rQ = req.query
+    const parseRequest = [
+        { "lat": rQ.ogLat.replace(/['"]+/g, ''), "len": rQ.ogLen.replace(/['"]+/g, '') },
+        { "lat": rQ.dtLat.replace(/['"]+/g, ''), "len": rQ.dtLen.replace(/['"]+/g, '') }
+    ]
+    const distance = await getDistance(parseRequest[0], parseRequest[1])
+    res.json({"distance": distance})
+})
  
 module.exports = router;
