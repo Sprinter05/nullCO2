5ea39f30a6b365d534921e39f4406f97b5a8e21f
Sprinter05 <sprinterhr@gmail.com>
2024-02-17 12:27:12 +0100

    more debug

--- route-api/routes/geos.js
+++ route-api/routes/geos.js
@@ -2,6 +2,7 @@ const nodeGeocoder = require('node-geocoder')
 const geolib = require('geolib')
 const amadeusAPI = require('amadeus');
 const { quickSort, ArraytoJSON, JSONtoArray } = require('./utils');
+const errJSON = {"error": "Request could not be fulfilled"}
 
 var geocoder = nodeGeocoder({provider: 'openstreetmap'})
 var amadeus = new amadeusAPI({
@@ -10,16 +11,16 @@ var amadeus = new amadeusAPI({
 });
 
 exports.getPlaceInfo = async function(origin){
-    const result = await geocoder.geocode(origin)
+    var result
+    try {
+        result = await geocoder.geocode(origin)
+    } catch(e) {console.log('[-] ERROR on getPlaceInfo' + '\n' + e)}
     const jsonOutput = {
         "lat": `${result[0].latitude}`,
         "lon": `${result[0].longitude}`,
         "loc": `${result[0].formattedAddress}`
     }
     console.log(`[+] NEW getPlaceInfo for ${origin}`)
-    if(jsonOutput === undefined || jsonOutput === null){
-        console.log(`[-] UNDEFINED on getPlaceInfo`)
-    }
     return jsonOutput
 }
 
@@ -29,18 +30,12 @@ exports.getDistance = async function(og, dt){
         { latitude: dt.lat, longitude: dt.len }
     )
     console.log(`[+] NEW getDistance for ${og}, ${dt}`)
-    if(distMeters === undefined || distMeters === null){
-        console.log(`[-] UNDEFINED on getDistance`)
-    }
     return distMeters
 }
 
 exports.getMiddle = function(points){
     const midPoint = geolib.getCenter(points)
     console.log(`[+] NEW getMiddle for n=${points.length} size`)
-    if(midPoint === undefined || midPoint === null){
-        console.log(`[-] UNDEFINED on getMiddle`)
-    }
     return midPoint
 }
 
@@ -48,9 +43,10 @@ exports.getAirport = async function(lat, len){
     var airport
     try {
         airport = await amadeus.referenceData.locations.airports.get({longitude: len, latitude: lat})
-    } catch (e) { console.log(e) }
+    } catch (e) { {console.log('[-] ERROR on getAirport' + '\n' + e)} }
 
     const maxDatum = Object.keys(airport.data).length
+    if (maxDatum === 0) return errJSON
     const top = 3
     var indexDist = {}
         
@@ -81,9 +77,6 @@ exports.getAirport = async function(lat, len){
     }
         
     console.log(`[+] NEW getAirport for ${lat}, ${len}`)
-    if(outputJson === undefined || outputJson === null){
-        console.log(`[-] UNDEFINED on getAirport`)
-    }
     return outputJson;
 }
 
@@ -93,7 +86,7 @@ async function getPrices(jsonInput){
         retQ = await amadeus.shopping.flightOffers.pricing.post(JSON.stringify(jsonInput))
         return retQ 
     } catch(e){
-        console.log(e)
+        {console.log('[-] ERROR on getPrices' + '\n' + e)}
         return null
     }
 }
@@ -107,7 +100,7 @@ exports.getFlight = async function(ogIata, dtIata, date){
             "departureDate": date,
             "adults": '1'
         })
-    } catch (e) {console.log(e)}
+    } catch (e) {{console.log('[-] ERROR on getFlight' + '\n' + e)}}
     const top = 5
     var fullJSON = {}
 
@@ -135,9 +128,6 @@ exports.getFlight = async function(ogIata, dtIata, date){
         }
     } 
     console.log(`[+] NEW getFlight from ${ogIata}} to ${dtIata} on ${date}`)
-    if(fullJSON === undefined || fullJSON === null){
-        console.log(`[-] UNDEFINED on getFlight`)
-    }
     return fullJSON
     
 }
